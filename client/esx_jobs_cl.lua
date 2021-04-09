b72ae9eab7c89b0123836dd1149e79073a44ed57
InVeK94 <68790635+InVeK94@users.noreply.github.com>
2021-04-09 21:54:32 +0200

    Create esx_jobs_cl.lua

---
+++ client/esx_jobs_cl.lua
@@ -0,0 +1,4 @@
+print("Jobs By InVek#9646")
+print("Jobs By InVek#9646 For Développement")
+print("Jobs By InVek#9646 For Développement https://discord.gg/kDdUTQzwZd")
+print("Jobs By InVek#9646 For Développement https://discord.gg/kDdUTQzwZd Rejoin ^^")
